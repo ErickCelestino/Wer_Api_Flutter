e321e0b1b14bf13a47138b74ead04504d379a6fb
PuraPreguica <erickcelestimo@gmail.com>
2023-03-05 17:01:06 -0300

    Adicionando a funcionalidade refresh

--- lib/screens/home_screen/widgets/journal_card.dart
+++ lib/screens/home_screen/widgets/journal_card.dart
@@ -6,7 +6,12 @@ import 'package:uuid/uuid.dart';
 class JournalCard extends StatelessWidget {
   final Journal? journal;
   final DateTime showedDate;
-  const JournalCard({Key? key, this.journal, required this.showedDate})
+  final Function refreshFunction;
+  const JournalCard(
+      {Key? key,
+      this.journal,
+      required this.showedDate,
+      required this.refreshFunction})
       : super(key: key);
 
   @override
